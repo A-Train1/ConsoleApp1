caf8413fc60380efd811119d15bab8197217ca3b
Администратор <@COMP69>
2021-11-21 14:51:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyClass1
+    {
+        public int donatorInt;
+    }
+
+    class MyClass2
+    {
+        public void GetRefOut(ref int recipientInt) {
+            Console.WriteLine("Value: " + recipientInt);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass1 mclass = new MyClass1();
+            mclass.donatorInt = 10;
+
+        }
+    }
+}
