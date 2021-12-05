f131ee90f495ca8de12ba75e506a623c2ae21b18
Артем Слепов <unity3user@gmail.com>
2021-12-05 14:49:14 +0500

    Start making whats told

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,15 +8,15 @@ namespace ConsoleApp1
 {
     class Program
     {
+        private delegate void ArgumentHandler();
+        private Dictionary<string, ArgumentHandler> argsList;
         static void Main(string[] args)
         {
-            string str = Console.ReadLine();
-            List<string> substrings = new List<string>(str.Split(' '));
-            foreach(string s in substrings)
-            {
-                Console.WriteLine(s);
-            }
-            System.Threading.Thread.Sleep(2000);
+            
         }
+    }
+    class ConfigKeeper
+    {
+
     }
 }
